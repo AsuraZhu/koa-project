b774a464a851ab9cfa391428fc31c87290c69b2e
zmm <1614180462@qq.com>
2018-01-17 17:57:07 +0800

    测试日志

--- app.js
+++ app.js
@@ -37,6 +37,12 @@ app.use(async (ctx, next) => {
   }
   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
 })
+// app.use(async (ctx, next) => {
+//   const start = new Date()
+//   await next();
+//   const ms = new Date() - start;
+//   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+// })
 
 // routes
 // app.use(index.routes(), index.allowedMethods())
