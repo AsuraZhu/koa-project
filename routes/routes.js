9cd4187b1d4d22134c2f64970f48573215038323
zmm <1614180462@qq.com>
2018-01-20 10:55:27 +0800

    删除测试文件

--- routes/routes.js
+++ routes/routes.js
@@ -2,12 +2,7 @@ const router = require("koa-router")();
 const connect = require('koa-connect');
 const index = require("./index")
 const user =  require("./users")
-const jsonServer = require('json-server')
-const server = jsonServer.create();
-server.use(jsonServer.defaults());
-const jsonRoute = jsonServer.router('db.json');
 
-router.use('/api', connect(server));
 router.use("/users" , index.routes(), index.allowedMethods())
 router.use("/", user.routes(), user.allowedMethods())
 module.exports = router
